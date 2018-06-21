93799f4691a0fed449a1e322cbbfe6aaef662c2d
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-06-21 08:08:03 -0400

    Added dependency checking

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -31,3 +31,6 @@ using System.Runtime.InteropServices;
 //
 //[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: KSPAssemblyDependency("ClickThroughBlocker", 1, 0)]
+[assembly: KSPAssemblyDependency("ToolbarController", 1, 0)]
